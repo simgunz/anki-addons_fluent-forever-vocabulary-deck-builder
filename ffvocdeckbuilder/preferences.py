576108f6edf9c519b74db414c96c7c96f0e5a83b
Simone Gaiarin <simgunz@gmail.com>
2016-01-10 21:07:56 +0100

    Load list of languages in combo boxes

--- ffvocdeckbuilder/preferences.py
+++ ffvocdeckbuilder/preferences.py
@@ -10,9 +10,14 @@ class Preferences(QDialog):
 
     def __init__(self, mw):
         QDialog.__init__(self, mw)
+        self.mw = mw
         #Dynamically loads the ui
         uic.loadUi(mw.pm.addonFolder() + '/ffvocdeckbuilder/ui/preferences.ui', self);
         #These two instructions run the dialog as modal dialog
+        self.loadLanguageCodes()
+        languages = sorted(self.languageCodes.values())
+        self.cbPreferredLanguage.addItems(languages)
+        self.cbSecondaryLanguage.addItems(languages)
         self.setModal(True)
         self.exec_()
 
@@ -21,3 +26,12 @@ class Preferences(QDialog):
 
     def reject(self):
         self.done(1)
+
+    def loadLanguageCodes(self):
+        self.languageCodes = {}
+        fileName = u"{0}/ffvocdeckbuilder/files/iso-639-1-language-codes" \
+            .format(self.mw.pm.addonFolder())
+        with open(fileName) as f:
+            for line in f:
+                (key, val) = line.split(',')
+                self.languageCodes[key] = val.rstrip('\n')
