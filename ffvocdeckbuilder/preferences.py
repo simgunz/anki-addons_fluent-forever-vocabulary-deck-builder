28e6b9f9d9d153946e7bcf7cec751aab66d4e348
Simone Gaiarin <simgunz@gmail.com>
2016-01-10 21:39:49 +0100

    Load and save configuration in ConfigObj
    ConfigObj saves the configuration in ini-like files but it supports subsections and advanced features

--- ffvocdeckbuilder/preferences.py
+++ ffvocdeckbuilder/preferences.py
@@ -2,6 +2,8 @@
 # Copyright: Simone Gaiarin <simgunz@gmail.com>
 # License: GNU GPL, version 3 or later; http://www.gnu.org/licenses/gpl.html
 
+from configobj import ConfigObj
+
 from aqt.qt import *
 #FIXME: Is uic present in default installation?
 from PyQt4 import uic
@@ -19,9 +21,28 @@ class Preferences(QDialog):
         self.cbPreferredLanguage.addItems(languages)
         self.cbSecondaryLanguage.addItems(languages)
         self.setModal(True)
+
+        #Load user config
+        self.user = self.mw.pm.name
+        self.config = ConfigObj('ffvdb.ini')
+        if self.config.has_key(self.user ):
+            self.leApiForvo.setText(self.config[self.user ]['APIs']['forvo'])
+            self.leApiBing.setText(self.config[self.user ]['APIs']['bing'])
+            self.cbPreferredLanguage.lineEdit().setText(self.config[self.user ]['Languages']['Primary'])
+            self.cbSecondaryLanguage.lineEdit().setText(self.config[self.user ]['Languages']['Secondary'])
+
         self.exec_()
 
     def accept(self):
+        if not self.config:
+            self.config = ConfigObj('ffvdb.ini')
+        if not self.config.has_key(self.user ):
+            self.config[self.user ] = {'APIs': {}, 'Languages': {}}
+        self.config[self.user ]['APIs']['forvo'] = self.leApiForvo.text()
+        self.config[self.user ]['APIs']['bing'] = self.leApiBing.text()
+        self.config[self.user ]['Languages']['Primary'] = self.cbPreferredLanguage.currentText()
+        self.config[self.user ]['Languages']['Secondary'] = self.cbSecondaryLanguage.currentText()
+        self.config.write()
         self.done(0)
 
     def reject(self):
