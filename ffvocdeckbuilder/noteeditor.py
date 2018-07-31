e0a2b763ef3192204d530a1cb482a3c4018f4d6d
Simone Gaiarin <simgunz@gmail.com>
2018-07-31 11:19:57 +0200

    Save browser reference in constructor

--- ffvocdeckbuilder/noteeditor.py
+++ ffvocdeckbuilder/noteeditor.py
@@ -42,6 +42,7 @@ class NoteEditor(object):
     def __init__(self, editor):
         self.editor = editor
         self.mw = self.editor.mw
+        self.browser = aqt.dialogs.open("Browser", self.mw)
         self.web = editor.web
         self.currentWord = ''
         self.preloadedNotesIds = list()
